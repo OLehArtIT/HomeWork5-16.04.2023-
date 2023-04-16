eef4dc5e7bc3edd7c0e6a731774b2165caf7764e
OlegArt <artoleg2003@gmail.com>
2023-04-16 22:29:06 +0200

    16.04.2023

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let numberOne = prompt ('Введите первое число');
+let numberTwo = prompt ('Введите второе число');
+let numberThree = prompt ('Введите третие число');
+numberOne = Number(numberOne);
+numberTwo = Number(numberTwo);
+numberThree = Number(numberThree);
+const resultNum = (numberOne + numberTwo + numberThree)/3;
+alert ('Среднее арифметическое: '+ resultNum);
